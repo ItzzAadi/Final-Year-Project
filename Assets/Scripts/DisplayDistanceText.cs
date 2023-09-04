27a31d095a9b1497278a0988759614c0f26c1bf6
Aadi Trivedi <nikita2015trivedi@gmail.com>
2023-09-04 20:27:08 +0530

    Added Distance Calculator
    Added the distance calculator, and it updates the TextMeshPro UI which is under the Fuel Canister

--- Assets/Scripts/DisplayDistanceText.cs
+++ Assets/Scripts/DisplayDistanceText.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEditor.Search;
 
 public class DisplayDistanceText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private TextMeshProUGUI _distanceText;
+    [SerializeField] private Transform _playerTrans;
+
+    private Vector2 _startPosition;
+
+    private void Start(){
+        _startPosition = _playerTrans.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        Vector2 distance = (Vector2)_playerTrans.position - _startPosition;
+        distance.y = 0f;
+
+        if (distance.x < 0){
+            distance.x = 0;
+        }
+        _distanceText.text = distance.x.ToString("F0") + "m"; //F0 so that no decimals shows
     }
 }
