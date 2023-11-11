cd4b2594b2d429b9187fbda01a5b76955946a4e0
Aadi Trivedi <nikita2015trivedi@gmail.com>
2023-11-11 16:37:05 +0530

    Working on AI

---
+++ Assets/Scripts/AgentAI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine.UIElements;
+
+public class AgentAI : Agent{
+
+    public override void CollectObservations(VectorSensor sensor){
+        sensor.AddObservation(transform.position);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions){
+        Debug.Log(actions.DiscreteActions[0]);
+    }
+
+}
