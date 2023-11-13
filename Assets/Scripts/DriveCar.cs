441c8fd1857ba0c1621b644514564731b89dffc6
Aadi Trivedi <nikita2015trivedi@gmail.com>
2023-11-13 14:50:36 +0530

    Changed teh speed and rotation

--- Assets/Scripts/DriveCar.cs
+++ Assets/Scripts/DriveCar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents.Integrations.Match3;
 using UnityEngine;
 
 public class DriveCar : MonoBehaviour
@@ -7,15 +8,16 @@ public class DriveCar : MonoBehaviour
     [SerializeField] private Rigidbody2D _frontTireRB;
     [SerializeField] private Rigidbody2D _backTireRB;
     [SerializeField] private Rigidbody2D _carRBCheckingIfThisWillSHow;
-    [SerializeField] private float _speed = 150f;
-    [SerializeField] private float _rotationSpeed = 300f;
-    private float _moveInput;
+    [SerializeField] private float _speed = 150f; //7
+    [SerializeField] private float _rotationSpeed = 300f; //40
+    private float __moveInput;
     
     private void Update(){
-        _moveInput = Input.GetAxisRaw("Horizontal");
+        __moveInput = Input.GetAxisRaw("Horizontal");
+        Move(__moveInput);
     }
 
-    private void FixedUpdate(){
+    private void Move(float _moveInput){
         _frontTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
         _backTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
         _carRBCheckingIfThisWillSHow.AddTorque(_moveInput * _rotationSpeed * Time.fixedDeltaTime);
